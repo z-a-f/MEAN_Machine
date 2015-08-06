1b3793a58f0f1fc4235f57cf3d0634b7607ce89d
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-08-06 10:49:55 -0400

    Sorta finished Node and Express

--- route-server/server.js
+++ route-server/server.js
@@ -14,6 +14,9 @@ app.get('/', function(req, res) {
 // Get an instance of the router
 var adminRouter = express.Router();
 
+/**********************/
+/***** Middleware *****/
+
 // route middleware that will happen on every request
 adminRouter.use(function(req, res, next) {
 	// log each request to the console
@@ -21,14 +24,33 @@ adminRouter.use(function(req, res, next) {
 	next(); // Continue
 });
 
-// admin main page; the dashboard (localhost:8000/admin)
+adminRouter.param('name', function(req, res, next, name) {
+	// do valifdation here
+	// log so we would know it workd
+	console.log('Validating name: ' + name);
+
+	// Once validation is done, save the item in the req
+	req.name = name
+
+	next();
+});
+
+/*********************/
+/***** Frontware *****/
+
+// admin main page; the dashboard (localhost:3000/admin)
 adminRouter.get('/', function(req, res) {
 	res.send('I am using dashboard!')
 });
 
 // users page (localhost:8000/admin/users)
 adminRouter.get('/users', function(req, res) {
-	res.send('I show all the users!');
+	res.send('No blank user!');
+});
+
+// users page (localhost:8000/admin/users)
+adminRouter.get('/users/:name', function(req, res) {
+	res.send('Hello ' + req.name + '!');
 });
 
 adminRouter.get('/posts', function(req, res) {
@@ -37,6 +59,19 @@ adminRouter.get('/posts', function(req, res) {
 
 app.use('/admin', adminRouter);
 
+app.route('/login')
+	// Show the form using GET http://localhost:3000/login
+	.get(function(req, res) {
+		res.send('This is the login form');
+	})
+	// Process the form using POST http://localhost:3000/login
+	.post(function(req, res) {
+		console.log('Processing...');
+		res.send('Processing the login form!');
+	});
+
 // start server:
-app.listen(8000);
-console.log('Port 8000');
+app.listen(3000);
+console.log('Listening on port 3000');
+
+
